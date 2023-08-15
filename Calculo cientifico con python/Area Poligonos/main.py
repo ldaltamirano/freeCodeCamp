3daafb5e1d370c55262c0bb823ef359ff4a5aea9
ldaltamirano <lalt123@gmail.com>
2023-08-15 12:55:37 -0300

    Se resolvio el problema del area de poligonos

--- Calculo cientifico con python/Area Poligonos/main.py
+++ Calculo cientifico con python/Area Poligonos/main.py
@@ -3,18 +3,24 @@ import shape_calculator
 from unittest import main
 
 
-rect = shape_calculator.Rectangle(5, 10)
-print(rect.get_area())
-rect.set_width(3)
-print(rect.get_perimeter())
-print(rect)
+rect = shape_calculator.Rectangle(7,3)
+# # print(rect.get_area())
+# # rect.set_width(3)
+# # print(rect.get_perimeter())
+# # print(rect)
+# print(rect.get_picture())
 
-sq = shape_calculator.Square(9)
-print(sq.get_area())
-sq.set_side(4)
-print(sq.get_diagonal())
-print(sq)
+sq = shape_calculator.Square(2)
+# # # print(sq.get_area())
+# # # sq.set_side(4)
+# # # print(sq.get_diagonal())
+# # # print(sq)
+# print(sq.get_picture())
+# # print(rect.get_amount_inside(sq))
+# print(sq.str())
 
 
-# Run unit tests automatically
+
+# Run unit tests automacls
+# tically
 main(module='test_module', exit=False)
--- Calculo cientifico con python/Area Poligonos/shape_calculator.py
+++ Calculo cientifico con python/Area Poligonos/shape_calculator.py
@@ -1,6 +1,78 @@
 class Rectangle:
+    width = 0
+    height = 0
+    def __init__(self, width, height):
+        self.width = width
+        self.height = height
+    def __str__(self):
+        return "Rectangle(width="+ str(self.width) +", height="+ str(self.height) +")"
+    def set_width(self, width):
+       self.width = width
+    
+    def set_height(self, height):
+       self.height = height
+    def get_area(self):
+        return self.width * self.height
+    def get_perimeter(self):
+        return 2*self.width + 2*self.height
+    def get_diagonal(self):
+        return (self.width**2 + self.height**2)**0.5
+    def get_picture(self):
+        if(self.width > 50 or self.height > 50):
+            return "Too big for picture."
+        if(self.width == 1 and self.height == 1 ):
+            return "*"
+        else:
+            limitSup = "*"*self.width+ '\n'
+            limitInf = "*"*self.width+ '\n'
+            rows = ""
+            index = 1
+
+            if(self.width == 2 and self.height == 2):
+                rows = ""
+            elif(self.width-2 == 1):
+                while(self.height -1 != index):
+                    rows += "***" + '\n'
+                    index+=1
+            elif(self.height-2 == 1):
+                rows += "*"+ "*" * (self.width-2) + "*" + '\n'        
+            else:
+                while(self.height-2 != index):
+                    rows += "*"+ "*" * (self.width-2) + "*" + '\n'        
+            return limitSup + rows + limitInf
+
+    
+    def get_amount_inside(self, shape):
+        print(shape)
+        if(shape.width > self.width or shape.height > self.height):
+            return 0
+        else:
+            countWidth = 0
+            countHeight = 0
+            instanceWidth = self.width
+            instanceHeight = self.height
+            shapeWidth = shape.width
+            shapeHeight = shape.height
+            while(instanceWidth >= shapeWidth):
+                countWidth += 1
+                instanceWidth -= shapeWidth
+            while(instanceHeight >= shapeHeight):
+                countHeight += 1
+                instanceHeight -= shapeHeight
+            
+            if(countWidth == 0 or countHeight == 0):
+                return 0
+            else:
+                return countWidth * countHeight
 
 
-
-
-class Square:
+class Square(Rectangle):
+    def __init__(self, lado):
+        self.width = lado
+        self.height = lado
+    def __str__(self):
+        return "Square(side="+ str(self.width) +")"
+    
+    def set_side(self, lado):
+       self.width = lado
+       self.height = lado
